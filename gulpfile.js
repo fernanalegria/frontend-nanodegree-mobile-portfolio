0fb8a9591bf732d7028502e2c4cfb8530fc8936f
Fernando Alegría <fernanalegria@gmail.com>
2018-06-22 08:53:33 +0200

    Website optimization: images resized

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
