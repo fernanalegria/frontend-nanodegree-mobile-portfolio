deede64542b8b7bbd71a7aa36fdcad8a407b7edf
falegria <fernando.miguel.alegria.medina@es.ey.com>
2018-06-23 20:03:15 +0200

    Website optimization: minify HTML, CSS and JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,45 @@
 'use strict';
 
-var autoprefixer = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
-var del = require('del');
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
-var runSequence = require('run-sequence');
-var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Gulp task to minify CSS files
+gulp.task('styles', function () {
+    return gulp.src('./css/*.css')
+        .pipe(csso())
+		.pipe(rename({
+		    suffix: '.min'
+		}))
+        .pipe(gulp.dest('./css_min'));
+});
+
+// Gulp task to minify JavaScript files
+gulp.task('scripts', function() {
+    return gulp.src('./js/*.js')
+        .pipe(uglify())
+		.pipe(rename({
+		    suffix: '.min'
+		}))
+        .pipe(gulp.dest('./js_min'));
+});
+
+// Gulp task to minify HTML files
+gulp.task('pages', function() {
+    return gulp.src('./index_unmin.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+		.pipe(rename('./index.html'))
+        .pipe(gulp.dest('./'));
+});
+
+// Gulp task to minify all files
+gulp.task('default', gulp.parallel(
+    'styles',
+    'scripts',
+    'pages'
+));
